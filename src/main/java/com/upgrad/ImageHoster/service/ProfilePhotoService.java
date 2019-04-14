c68aa0973d0bdb7365a5472c8275fccdf9dba047
vikasj018 <vikasjaiprakash.18@gmail.com>
2019-04-14 19:40:41 +0530

    First Commit

---
+++ src/main/java/com/upgrad/ImageHoster/service/ProfilePhotoService.java
@@ -0,0 +1,10 @@
+package com.upgrad.ImageHoster.service;
+
+import com.upgrad.ImageHoster.model.ProfilePhoto;
+
+public interface ProfilePhotoService {
+    void save(ProfilePhoto image);
+
+    void update(ProfilePhoto image);
+
+}
---
+++ src/main/java/com/upgrad/ImageHoster/service/UserService.java
@@ -0,0 +1,15 @@
+package com.upgrad.ImageHoster.service;
+
+import com.upgrad.ImageHoster.model.User;
+
+public interface UserService {
+    User login(String username, String password);
+
+    User getByName(String username);
+
+    User getByNameWithProfilePhoto(String username);
+
+    boolean register(User user);
+
+    void update(User user);
+}
